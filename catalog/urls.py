fc8f3b53b07b81cdc0e294ce8ea7ed9dfe9dc9a1
sylvester <nkwontasylvester15@gmail.com>
2022-01-22 11:26:00 -0900

    book_detail.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
      path('books/', views.BookListView.as_view(), name='books'),
      path('books/<slug:slug>/', views.BookDetailView.as_view(), name='book-detail'),
      path('author/', views.AuthorListView.as_view(), name='authors'),
-     path('author/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail')
+     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     
 ]
