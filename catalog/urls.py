fed93c85a2b0ab70b27bdd4d2917170c156e3d23
sylvester <nkwontasylvester15@gmail.com>
2022-01-22 08:54:55 -0900

    views.py config

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from catalog import views
 
 urlpatterns = [
+
+     path('', views.index, name='index'),
     
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from catalog.models import *
 # Create your views here.
+
+def index(request):
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.count()
+    num_instance = BookInstance.objects.count()
+
+    #Availaible books with the status('a')
+    num_instance_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    #Number of authors
+    num_authors = Author.objects.count()
+    #num_genre = Genre.objects.count()
+
+    context = {
+
+        'num_books': num_books, 
+        'num_instance': num_instance,
+        'num_instance_available': num_instance_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'catalog/index.html', context)
+
+
+
+
+
